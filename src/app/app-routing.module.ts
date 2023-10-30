6a8f110d923ec54bc3371a9eae3a279bd37b3697
teamflp <teamflp@yahoo.it>
2023-10-30 02:16:34 +0100

    first commit for cv angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AboutComponent} from "./myComponents/about/about.component";
+import { ContactComponent } from './myComponents/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'about', component: AboutComponent }, // Attention pas de /about. Plutot about tout cours.
+   { path: 'contact', component: ContactComponent},
+   { path: '', redirectTo: '/', pathMatch: 'full' }, // default route
+   { path: '**', redirectTo: '/home' } // wildcard route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './myComponents/header/header.component';
+import { HomeComponent } from './myComponents/home/home.component';
+import { AboutComponent } from './myComponents/about/about.component';
+import { FooterComponent } from './myComponents/footer/footer.component';
+import { ContactComponent } from './myComponents/contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    FooterComponent,
+    ContactComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatIconModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
