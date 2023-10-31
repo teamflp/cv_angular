3ab3fd596931650528c789050c26f952388ea11e
teamflp <teamflp@yahoo.it>
2023-10-31 19:00:52 +0100

    Ajout des liens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AboutComponent} from "./myComponents/about/about.component";
+import { CompetenceComponent } from './myComponents/competence/competence.component';
 import { ContactComponent } from './myComponents/contact/contact.component';
+import {ExperienceComponent} from "./myComponents/experience/experience.component";
+import { FormationComponent } from './myComponents/formation/formation.component';
 
 const routes: Routes = [
    { path: 'about', component: AboutComponent }, // Attention pas de /about. Plutot about tout cours.
+   { path: 'experience', component: ExperienceComponent},
+   { path: 'competence', component: CompetenceComponent},
+   { path: 'formation', component: FormationComponent},
    { path: 'contact', component: ContactComponent},
    { path: '', redirectTo: '/', pathMatch: 'full' }, // default route
    { path: '**', redirectTo: '/home' } // wildcard route
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { FooterComponent } from './myComponents/footer/footer.component';
 import { ContactComponent } from './myComponents/contact/contact.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from "@angular/material/icon";
+import { ExperienceComponent } from './myComponents/experience/experience.component';
+import { CompetenceComponent } from './myComponents/competence/competence.component';
+import { FormationComponent } from './myComponents/formation/formation.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import {MatIconModule} from "@angular/material/icon";
     HomeComponent,
     AboutComponent,
     FooterComponent,
-    ContactComponent
+    ContactComponent,
+    ExperienceComponent,
+    CompetenceComponent,
+    FormationComponent
   ],
     imports: [
         BrowserModule,
